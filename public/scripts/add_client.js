367302ea99ae1c182ff1d242946b7f6d67f00a60
AF-Jama <Abdifatah.jama01@protonmail.com>
2022-07-18 01:02:53 +0100

    Refactored client side js for adding site password against use account and also refactored add password controller

--- public/scripts/add_client.js
+++ public/scripts/add_client.js
@@ -2,18 +2,24 @@ const addSitePasswordForm = document.getElementById('add-site-password-form')
 const SiteNameInput = document.getElementById('form-control-site-name')
 const sitePasswordInput = document.getElementById('form-control-site-password')
 
+const xmlhttp = new XMLHttpRequest()
+
 addSitePasswordForm.addEventListener('submit',e=>{
+    e.preventDefault()
+
     const SiteNameInputValue = SiteNameInput.value.trim()
     const sitePasswordInputValue = sitePasswordInput.value.trim()
-    if (SiteNameInputValue.isEmpty() && sitePasswordInputValue.isEmpty()){
-        return window.location.href = '/mainpage/add'
-    }
+    console.log(SiteNameInputValue)
+    console.log(sitePasswordInputValue)
+    // if (SiteNameInputValue.isEmpty() && sitePasswordInputValue.isEmpty()){
+    //     return window.location.href = '/mainpage/add'
+    // }
     xmlhttp.open("POST",'/mainpage/add')
     xmlhttp.setRequestHeader('Content-Type', 'application/json');
     xmlhttp.setRequestHeader('Accept', 'application/json');
     xmlhttp.send(JSON.stringify({
         siteName:SiteNameInputValue,
-        Password:sitePasswordInputValue
+        sitePassword:sitePasswordInputValue
     }))
     xmlhttp.onreadystatechange = function(){
         console.log(xmlhttp.status)
